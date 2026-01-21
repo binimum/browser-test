0bc7a24fd78251cb605b0312bb35d7d06b435443
binimum <uimax.bai@gmail.com>
2026-01-21 13:24:10 +0000

    Fix build script and suppress chunk warning

--- README.md
+++ README.md
@@ -4,8 +4,8 @@
 
 1. Install packages `npm i`
 2. Run dev server `npm run dev`
-4. Static assets are stored at `public/` and are avaliable at `/` on the web
-5. All TS is in `src/`
+3. Build for Cloudflare Workers `npm run build`
+4. Preview the build `npm run preview`
+5. Static assets are stored at `public/` and are avaliable at `/` on the web
 
 ### Create a new test
-
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import legacy from '@vitejs/plugin-legacy';
-// import pluginPurgeCss from "@mojojoejo/vite-plugin-purgecss";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-export default {
-  plugins: [
-    legacy({
-      targets: ['defaults', 'not IE 11'],
-    }),
-  ],
-}
+export default defineConfig({
+    plugins: [sveltekit()],
+    build: {
+        chunkSizeWarningLimit: 1500,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules/caniuse-lite')) {
+                        return 'caniuse';
+                    }
+                },
+            },
+        },
+    },
+});
